b64d5d86e77f3d70aaa74356b0355325f895eff8
zhiyan114 <zhiyan114@furries.dev>
2025-10-17 00:59:23 -0400

    Fix some lint issues lmao

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,13 @@
 import Express from "express";
-import type {Request, Response} from "express";
+import type { Request, Response } from "express";
 
 const app = Express();
 
 app.get("/", async (req: Request, res: Response)=>{
-    res.send("Hello World!");
-})
+  res.send("Hello World!");
+});
 
-const port = process.env["PORT"] ?? 8080
+const port = process.env["PORT"] ?? 8080;
 app.listen(port, ()=>{
-    console.log(`Running on http://localhost:${port}`)
-})
+  console.log(`Running on http://localhost:${port}`);
+});
