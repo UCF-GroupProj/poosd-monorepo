b4cf9a3bad97e3907f6c7fcf0794bcadc5a8c9e3
zhiyan114 <zhiyan114@furries.dev>
2025-10-17 00:07:01 -0400

    Almost Done LOL

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,13 @@
+import Express from "express";
+import type {Request, Response} from "express";
+
+const app = Express();
+
+app.get("/", async (req: Request, res: Response)=>{
+    res.send("Hello World!");
+})
+
+const port = process.env["PORT"] ?? 8080
+app.listen(port, ()=>{
+    console.log(`Running on http://localhost:${port}`)
+})
