66f17af3f98e233732f154af3747a0e514f3e1cb
zhiyan114 <zhiyan114@furries.dev>
2025-10-20 16:34:19 -0400

    Backend: Integrate release flag

--- apps/backend/src/sentry.ts
+++ apps/backend/src/sentry.ts
@@ -3,6 +3,7 @@ import sentry, { consoleLoggingIntegration } from "@sentry/node";
 sentry.init({
   dsn: process.env["BACKEND_SENTRY_DSN"],
   environment: process.env["ENVIRONMENT"],
+  release: `${process.env["ENVIRONMENT"]?.slice(0,4) ?? "????"}-${process.env["RAILWAY_GIT_COMMIT_SHA"]?.slice(0,7) ?? "???????"}`,
   integrations: [
     consoleLoggingIntegration({ levels: ["error"] })
   ],
