dcf3bae6b63bdc3b68dd81d0efc134edefa616a9
zhiyan114 <zhiyan114@furries.dev>
2025-10-24 12:10:30 -0400

    Added JWT and email related stuff

--- apps/backend/src/CoreService.ts
+++ apps/backend/src/CoreService.ts
@@ -2,18 +2,28 @@ import Express, { type Express as ExpType } from "express";
 import { MongoClient, ServerApiVersion } from "mongodb";
 import type { RouteHandle } from "./routes";
 import { logger, setupExpressErrorHandler } from "@sentry/node";
+import { JWTManager } from "@repo/utils/JTWManager.ts";
+import { MailService } from "@repo/utils/MailService.ts";
 
 export class CoreService {
   private _webServer: ExpType;
   private _mongoCli: MongoClient;
+  private _JWTMGR: JWTManager;
+  private _emailSRV: MailService;
 
   constructor() {
     this._webServer = Express();
 
-    // MongoDB setup
+    // Check required env
     const mongoConnStr = process.env["MONGO_CONN"];
+    const emailAPIStr = process.env["EMAIL_KEY"];
     if(!mongoConnStr) throw new CoreServiceExcept("Missing MONGO_CONN env variable");
+    if(!emailAPIStr) throw new CoreServiceExcept("Missing EMAIL_KEY env variable");
+
+    // Initialize stuff
     this._mongoCli = new MongoClient(mongoConnStr, { serverApi: ServerApiVersion.v1 });
+    this._JWTMGR = new JWTManager();
+    this._emailSRV = new MailService(emailAPIStr);
   }
 
   public async setup(routes: (new(service: CoreService) => RouteHandle)[]) {
@@ -37,6 +47,14 @@ export class CoreService {
   get database() {
     const isProd = process.env["ENVIRONMENT"] === "prod";
     return this._mongoCli.db(isProd ? 'Olympull' : 'Olympull_dev');
+  }
+
+  get JWTMGR() {
+    return this._JWTMGR;
+  }
+
+  get emailAPI() {
+    return this._emailSRV;
   }
 }
 
