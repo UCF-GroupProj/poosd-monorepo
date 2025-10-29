e223374a02185e965c5b80cec3049870f538f619
zhiyan114 <zhiyan114@furries.dev>
2025-10-28 23:28:31 -0400

    Fixed frontend eslint issue

--- packages/eslint-config/frontend.mjs
+++ packages/eslint-config/frontend.mjs
@@ -1,52 +1,48 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    
-    rules: {
-      indent: ["error", 2, {
-        SwitchCase: 1,
-      }],
-      semi: ["error", "always"],
-      "no-async-promise-executor": "off",
-      "no-constant-condition": "off",
-      eqeqeq: ["error", "always"],
-      "no-trailing-spaces": ["error"],
-      "spaced-comment": [
-        "error",
-        "always",
-        {
-          "markers": ["!"]
-        }
-      ],
-      "object-curly-spacing": ["error", "always"],
-      "lines-between-class-members": [
-        'error',
-        'always',
-        { 'exceptAfterSingleLine': true }
-      ]
-    },
-  }),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.config({
+  rules: {
+    indent: ["error", 2, {
+      SwitchCase: 1,
+    }],
+    semi: ["error", "always"],
+    "no-async-promise-executor": "off",
+    "no-constant-condition": "off",
+    eqeqeq: ["error", "always"],
+    "no-trailing-spaces": ["error"],
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        "markers": ["!"]
+      }
     ],
-    
-  },
-];
+    "object-curly-spacing": ["error", "always"],
+    "lines-between-class-members": [
+      'error',
+      'always',
+      { 'exceptAfterSingleLine': true }
+    ]
+  }
+}), {
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ],
+  
+}];
 
 export default eslintConfig;
