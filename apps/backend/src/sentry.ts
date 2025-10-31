8374d5f6549fe57ad2c16ed0eeba5a089c11e7a6
zhiyan114 <zhiyan114@furries.dev>
2025-10-31 17:25:21 -0400

    Actual fix now lmfao

--- apps/backend/src/sentry.ts
+++ apps/backend/src/sentry.ts
@@ -4,9 +4,11 @@ sentry.init({
   dsn: process.env["BACKEND_SENTRY_DSN"],
   environment: process.env["ENVIRONMENT"],
   release: `${process.env["ENVIRONMENT"]?.slice(0,4) ?? "????"}-${process.env["RAILWAY_GIT_COMMIT_SHA"]?.slice(0,7) ?? "???????"}`,
+  beforeSend: (e) => ["SyntaxError"].includes(e.exception?.values?.[0]?.type ?? "") ? null : e, // Drop specific errors
   integrations: [
     consoleLoggingIntegration({ levels: ["error"] })
   ],
   enableLogs: true,
-  beforeSendLog: (log) => console.log(`[${log.level}: ${log.message}]`) ?? log
+  beforeSendLog: (log) => console.log(`[${log.level}: ${log.message}]`) ?? log,
+  ignoreErrors: ["SyntaxError"]
 });
