7428730d6a6542d45dcf89b8e7b88403d7109c19
zhiyan114 <zhiyan114@furries.dev>
2025-10-31 19:33:49 -0400

    checkAuth: Header checks

--- apps/backend/src/middleman.ts
+++ apps/backend/src/middleman.ts
@@ -1,4 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
 export function checkAuth(req: Request, res: Response, next: NextFunction) {
+  // Validate Token
+  if(!req.headers.authorization)
+    return res.status(401).send("Missing Authorization Header");
+
+  const [TokenType, Token] = req.headers.authorization.split(" ");
+  if(TokenType !== "Bearer")
+    return res.status(401).send("Invalid token type found! Maybe you forgot to include 'Bearer' prefix?");
+  if(!Token)
+    return res.status(401).send("Missing Actual Token...");
   next();
 }
