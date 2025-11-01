d90f2c18a6844960a0a38a14fb0c73fc772627a0
zhiyan114 <zhiyan114@furries.dev>
2025-11-01 13:22:25 -0400

    Potentially fix bad issue caused by circular dependencies

---
+++ apps/backend/src/routes/baseHandle.ts
@@ -0,0 +1,16 @@
+import type { CoreService } from "../CoreService";
+
+export type GetParams<Path extends string> = Path extends `/${infer Param}/${infer Rest}`
+  ? { [K in Param]: string } & GetParams<Rest>
+  : Path extends `/${infer Param}`
+  ? { [K in Param]: string }
+  : {}; // eslint-disable-line
+
+export abstract class RouteHandle {
+  protected coreSrv: CoreService;
+  constructor(service: CoreService) {
+    this.coreSrv = service;
+  }
+
+  abstract setup(): void;
+}
