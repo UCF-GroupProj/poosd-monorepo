4bfc8eac9cb66b97e65947a7378a5f442a03c98e
zhiyan114 <zhiyan114@furries.dev>
2025-11-03 13:36:51 -0500

    Complete getHandle

--- apps/backend/src/routes/emailVerification.ts
+++ apps/backend/src/routes/emailVerification.ts
@@ -1,23 +1,33 @@
 import { RouteHandle } from "./baseHandle";
 import type { Request, Response } from "express";
+import type { IAccountRequest } from "@repo/utils/types";
+import { logger } from "@sentry/node";
 
 export class emailVerification extends RouteHandle {
   public setup() {
     this.coreSrv.webServer.route("/verifyemail/:verifyCode")
-        .get(this.getHandle.bind(this))
-        .patch(this.patchHandle.bind(this));
+      .get(this.getHandle.bind(this))
+      .patch(this.patchHandle.bind(this));
     this.coreSrv.webServer.post("/verifyemail", this.postHandle.bind(this));
   }
 
-  async getHandle(req: Request, res: Response) {
-    return res.send("Hello World! :3");
+  async getHandle(req: Request<{verifyCode: string}>, res: Response) {
+    const DBColl = this.coreSrv.database.collection<IAccountRequest>("ResetRequest");
+
+    const credRes = await DBColl.findOne({ requestId: req.params.verifyCode });
+    if(!credRes) {
+      logger.info(logger.fmt`${req.params.verifyCode} request for email verification was not found`);
+      return res.status(404).send("Email Verification Code Not Found");
+    }
+
+    return res.send(credRes.userId.toHexString());
   }
 
   async postHandle(req: Request, res: Response) {
     return res.send("Hello World! :3");
   }
 
-  async patchHandle(req: Request, res: Response) {
+  async patchHandle(req: Request<{verifyCode: string}>, res: Response) {
     return res.send("Hello World! :3");
   }
 }
