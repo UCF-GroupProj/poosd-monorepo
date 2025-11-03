5b0e1448528882093512e4a0321553e1c18961b0
zhiyan114 <zhiyan114@furries.dev>
2025-11-03 16:06:00 -0500

    Finished patchHandle

--- apps/backend/src/routes/emailVerification.ts
+++ apps/backend/src/routes/emailVerification.ts
@@ -30,6 +30,7 @@ export class emailVerification extends RouteHandle {
   async postHandle(req: Request, res: Response<unknown, tokenData>) {
     const AccDBColl = this.coreSrv.database.collection<IUserInfo>("Users");
     const ReqDBColl = this.coreSrv.database.collection<IAccountRequest>("ResetRequest");
+
     // Check verification status
     const veriReq = await AccDBColl.findOne({ _id: new ObjectId(res.locals.id), requestType: "email" });
     if(!veriReq) {
@@ -84,6 +85,27 @@ export class emailVerification extends RouteHandle {
   }
 
   async patchHandle(req: Request<{verifyCode: string}>, res: Response) {
-    return res.send("Hello World! :3");
+    const AccDBColl = this.coreSrv.database.collection<IUserInfo>("Users");
+    const ReqDBColl = this.coreSrv.database.collection<IAccountRequest>("ResetRequest");
+
+    const verifyReq = await ReqDBColl.findOneAndDelete({ requestId: req.params.verifyCode });
+    if(!verifyReq) {
+      logger.info(logger.fmt`Email verification code ${req.params.verifyCode} was used to confirm verification but doesn't exist in DB`);
+      return res.status(404).send("Verfication email or code isn't valid");
+    }
+
+    // Update Status
+    const setVerifyReq = await AccDBColl.findOneAndUpdate({ _id: verifyReq.userId, verified: false }, { $set: { verified: true } });
+    if(!setVerifyReq) {
+      logger.error(logger.fmt`Request ${req.params.verifyCode} associated with already verified account??`);
+      return res.send(422).send("Account already verified??");
+    }
+
+    const cleanupReq = await ReqDBColl.deleteMany({ userId: setVerifyReq._id });
+    if(cleanupReq.acknowledged && cleanupReq.deletedCount > 0)
+      logger.warn(logger.fmt`User ${setVerifyReq._id.toHexString()} contains extra ${cleanupReq.deletedCount} email verification request??`);
+
+    return res.send("Email Successfully Verified");
+
   }
 }
