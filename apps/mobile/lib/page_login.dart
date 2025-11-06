5554d070d7ad0cd2b5e48cd0c4cc37289d4c9964
Lepro-C <imiskevich03@gmail.com>
2025-11-05 21:05:27 -0500

    Commented out login

---
+++ apps/mobile/lib/page_login.dart
@@ -0,0 +1,23 @@
+
+
+// import 'package:flutter/material.dart';
+// import 'package:large_project_dart/page_collections.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key})
+
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.amber,
+//       body: Column(
+        
+//       ),
+//     )
+//   }
+// }
