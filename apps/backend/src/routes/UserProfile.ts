b27632c27bec95f3b14499b97645f1910459e904
zhiyan114 <zhiyan114@furries.dev>
2025-11-08 16:03:49 -0500

    Added auth routes

--- apps/backend/src/routes/UserProfile.ts
+++ apps/backend/src/routes/UserProfile.ts
@@ -1,12 +1,14 @@
+import type { tokenData } from "@repo/utils/JTWManager.ts";
+import { AuthMWGen } from "../middleman";
 import { RouteHandle } from "./baseHandle";
 import type { Request, Response } from "express";
 
 export class Main extends RouteHandle {
   public setup() {
-    this.coreSrv.webServer.get("/profile", this.profileHandle.bind(this));
+    this.coreSrv.webServer.get("/profile", AuthMWGen(this.coreSrv.database), this.profileHandle.bind(this));
   }
 
-  async profileHandle(req: Request, res: Response) {
+  async profileHandle(req: Request, res: Response<unknown, tokenData>) {
     return res.send("Hello World! :3");
   }
 }
--- apps/backend/src/routes/cardRoute.ts
+++ apps/backend/src/routes/cardRoute.ts
@@ -1,17 +1,19 @@
+import type { tokenData } from "@repo/utils/JTWManager.ts";
+import { AuthMWGen } from "../middleman";
 import { RouteHandle } from "./baseHandle";
 import type { Request, Response } from "express";
 
 export class Main extends RouteHandle {
   public setup() {
-    this.coreSrv.webServer.get("/card/:cardID", this.getHandle.bind(this));
-    this.coreSrv.webServer.get("/card/summary", this.summaryHandle.bind(this));
+    this.coreSrv.webServer.get("/card/:cardID", AuthMWGen(this.coreSrv.database), this.getHandle.bind(this));
+    this.coreSrv.webServer.get("/card/summary", AuthMWGen(this.coreSrv.database), this.summaryHandle.bind(this));
   }
 
-  async getHandle(req: Request<{cardID: string}>, res: Response) {
+  async getHandle(req: Request<{cardID: string}>, res: Response<unknown, tokenData>) {
     return res.send("Hello World! :3");
   }
 
-  async summaryHandle(req: Request, res: Response) {
+  async summaryHandle(req: Request, res: Response<unknown, tokenData>) {
     return res.send("Hello World! :3");
   }
 }
